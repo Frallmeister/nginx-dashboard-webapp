7a8571639d0e1d5f09841497963485035cc9acec
Fredrik Hansson <fredda.hansson@gmail.com>
2023-06-05 16:59:35 +0200

    Preprocess new data every 10 sec

--- preprocess/app.py
+++ preprocess/app.py
@@ -1,7 +1,8 @@
 import time
-from sqlalchemy import create_engine
+import pandas as pd
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
-from models import Titanic, Base
+from models import Diamonds, Base
 
 user = "root"
 with open("/run/secrets/db-password") as fp:
@@ -14,4 +15,29 @@ engine = create_engine(f"mysql+pymysql://{user}:{passwd}@{host}/{dbname}?charset
 Base.metadata.create_all(engine)
 
 while True:
-    time.sleep(5)
+    raw_data = pd.read_csv("/data/raw_data.csv")
+    raw_data.drop("Unnamed: 0", axis=1, inplace=True)
+    # records = raw_data.to_dict("records")
+
+    stored = pd.read_sql(
+        "SELECT * FROM diamonds",
+        engine
+    )
+    stored.drop("id_", axis=1, inplace=True)
+
+    raw_data = pd.concat([raw_data, stored]).drop_duplicates()
+    records = raw_data.to_dict("records")
+
+    # Delete all records
+    with Session(engine) as session:
+        stmt = select(Diamonds)
+        for row in session.scalars(stmt):
+            session.delete(row)
+        session.commit()
+
+    # Add new records
+    with Session(engine) as session:
+        for record in records:
+            session.add(Diamonds(**record))
+        session.commit()
+    time.sleep(10)
--- preprocess/models.py
+++ preprocess/models.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 class Base(DeclarativeBase):
     pass
 
+"""
 class Titanic(Base):
     __tablename__ = "titanic"
 
@@ -26,3 +27,22 @@ class Titanic(Base):
 
     def __repr__(self):
         return f"<Titanic id={self.id_}, survived={self.survived}>"
+"""
+
+class Diamonds(Base):
+    __tablename__ = "diamonds"
+
+    id_: Mapped[int] = mapped_column(primary_key=True)
+    carat: Mapped[float] = mapped_column(nullable=True)
+    cut: Mapped[str] = mapped_column(String(255), nullable=True)
+    color: Mapped[str] = mapped_column(String(255), nullable=True)
+    clarity: Mapped[str] = mapped_column(String(255), nullable=True)
+    depth: Mapped[float] = mapped_column(nullable=True)
+    table: Mapped[float] = mapped_column(nullable=True)
+    price: Mapped[int] = mapped_column(nullable=True)
+    # x: Mapped[float] = mapped_column(nullable=True)
+    # y: Mapped[float] = mapped_column(nullable=True)
+    # z: Mapped[float] = mapped_column(nullable=True)
+
+    def __repr__(self):
+        return f"<Diamonds id={self.id_}, carat={self.carat}, price={self.price}>"
